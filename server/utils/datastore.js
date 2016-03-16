0548fec60fc13840a8a5e206a09f022f8a6e9524
Vietor Liu <vietor.liu@gmail.com>
2016-03-16 19:01:02 +0800

    Replace nonosql with bignosql

--- server/utils/datastore.js
+++ server/utils/datastore.js
@@ -1,31 +1,31 @@
 var config = require('config');
-var nonosql = require('./nonosql');
+var bignosql = require('bignosql');
 
-var nsclient = nonosql.connect(config.store.db.type, config.store.db[config.store.db.type]);
+var nsclient = bignosql.connect(config.store.db.type, config.store.db[config.store.db.type]);
 
 exports.CounterModel = nsclient.model("counters", {
-    _id: nonosql.STRING,
-    value: nonosql.NUMBER
+    _id: bignosql.STRING,
+    value: bignosql.NUMBER
 });
 
 exports.UserModel = nsclient.model("users", {
-    _id: nonosql.NUMBER
+    _id: bignosql.NUMBER
 });
 
 exports.TopicModel = nsclient.model("topics", {
-    _id: nonosql.NUMBER,
-    node_id: nonosql.NUMBER,
-    user_id: nonosql.NUMBER,
+    _id: bignosql.NUMBER,
+    node_id: bignosql.NUMBER,
+    user_id: bignosql.NUMBER,
     update_user_id: {
-        type: nonosql.NUMBER,
+        type: bignosql.NUMBER,
         default: 0
     },
-    create_date: nonosql.NUMBER
+    create_date: bignosql.NUMBER
 });
 
 exports.ReplyModel = nsclient.model("replies", {
-    _id: nonosql.NUMBER,
-    topic_id: nonosql.NUMBER,
-    user_id: nonosql.NUMBER,
-    create_date: nonosql.NUMBER
+    _id: bignosql.NUMBER,
+    topic_id: bignosql.NUMBER,
+    user_id: bignosql.NUMBER,
+    create_date: bignosql.NUMBER
 });
