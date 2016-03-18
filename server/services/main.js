6a5fa4ae076e5312bf1c02f1fe216ec1d1ba36e8
Vietor Liu <vietor.liu@gmail.com>
2016-03-18 18:54:08 +0800

    Add permission for trash node

--- server/services/main.js
+++ server/services/main.js
@@ -30,11 +30,19 @@ exports.home_recent = function(req, res) {
     });
 };
 
-exports.node = function(req, res) {
+function validateNode(req, res, callback) {
     var node_id = parseInt(req.param('id'));
     if (!node_id)
         res.redirect("/");
-    else
+    else if (node_id < 0 && !(req.session && req.session.login_user && req.session.login_user.role > 0))
+        common.sendAlter(res, "无访问此节点的权限");
+    else {
+        callback(node_id);
+    }
+}
+
+exports.node = function(req, res) {
+    validateNode(req, res, function(node_id) {
         brmx.topic_list(node_id, brcx.TOPIC_OTYPE_UPDATE, 0, config.limits.topic_pagesize, function(err, topics, user_map) {
             if (err)
                 common.sendAlter(res, err);
@@ -44,14 +52,12 @@ exports.node = function(req, res) {
                     topic_list: common.dumpTopicList(topics, user_map)
                 });
         });
+    });
 };
 
 exports.node_recent = function(req, res) {
-    var page = parseInt(req.param('page'));
-    var node_id = parseInt(req.param('id'));
-    if (!node_id)
-        res.redirect("/");
-    else
+    validateNode(req, res, function(node_id) {
+        var page = parseInt(req.param('page'));
         brmx.topic_list(node_id, brcx.TOPIC_OTYPE_CREATE, getOffset(page), config.limits.topic_pagesize, function(err, topics, user_map) {
             if (err)
                 common.sendAlter(res, err);
@@ -63,4 +69,5 @@ exports.node_recent = function(req, res) {
                     has_next: topics.length >= config.limits.topic_pagesize
                 });
         });
+    });
 };
