fef04cc7c1631fe00cfbf72e7841856e13618337
Vietor Liu <vietor.liu@gmail.com>
2016-04-02 21:23:00 +0800

    Add avatar store to qiniu

--- server/commons/avatar.js
+++ server/commons/avatar.js
@@ -1,5 +1,8 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require('fs'),
+    path = require('path'),
+    crypto = require('crypto');
+var xbase64 = require('xbase64');
+var request = require('request');
 var _ = require('underscore');
 var config = require('config');
 var uuid = require('node-uuid');
@@ -7,7 +10,7 @@ var uuid = require('node-uuid');
 var store_type = config.store.avatar.type;
 var store_params = config.store.avatar.params;
 
-if (!store_params || !_.contains(['localfs'], store_type)) {
+if (!store_params || !_.contains(['localfs', 'qiniu'], store_type)) {
     throw Error('Unsupported avatar store type');
 }
 
@@ -35,9 +38,41 @@ function mkdirp(filepath) {
     return filepath;
 }
 
+function hmac_sha1(content, secret) {
+    return crypto.createHmac('sha1', secret).update(content).digest();
+}
+
+function storeQiniu(params, filename, filepath, callback) {
+    if (!filename)
+        filename = path.basename(filepath);
+    var putPolicy = {
+        scope: params.bucket + ':' + filename,
+        deadline: Math.floor(Date.now() / 1000) + 3600,
+        returnBody: "{}"
+    };
+    var encodedPutPolicy = xbase64.urlencode(JSON.stringify(putPolicy));
+    var encodedSign = xbase64.urlencode(hmac_sha1(encodedPutPolicy, params.secretKey));
+    var formData = {
+        key: filename,
+        file: fs.createReadStream(filepath),
+        token: params.accessKey + ':' + encodedSign + ':' + encodedPutPolicy
+    };
+    request.post({
+        url: params.url,
+        formData: formData
+    }, function(err, resp, body) {
+        if (err)
+            callback(err);
+        else if (resp.statusCode == 200)
+            callback(null);
+        else
+            callback(new Error('status: ' + resp.statusCode) + ', message: ' + JSON.parse(body).error);
+    });
+}
+
 exports.storeAvatar = function(origin_filepath, callback) {
+    var filename = uuid.v1().replace(/-/g, '') + path.extname(origin_filepath);
     if (store_type == "localfs") {
-        var filename = uuid.v1().replace(/-/g, '') + path.extname(origin_filepath);
         var daypath = brcx.getDayCode('/').substring(0, 7);
         var rawpath = mkdirp(path.join(store_params.filepath, daypath));
         fileCopy(origin_filepath, path.join(rawpath, filename), function(err) {
@@ -46,5 +81,14 @@ exports.storeAvatar = function(origin_filepath, callback) {
             else
                 callback(null, daypath + '/' + filename);
         });
+    } else if (store_type == "qiniu") {
+        storeQiniu(store_params, filename, origin_filepath, function(err) {
+            if (err)
+                callback(brcx.errFSAccess(err));
+            else
+                callback(null, filename);
+        });
+    } else {
+        callback(brcx.errFSAccess('不支持的存储类型'));
     }
 };
